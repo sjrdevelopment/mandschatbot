fcd2bc4a1587781b4a3aa1a78b914a362d601432
sjrdevelopment <stuj.robinson@gmail.com>
2019-10-22 21:29:22 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# mandschatbot
+# Team POLOMINT chatbot project
+
+## UI
+The UI project is contained in `/ui`.
+
+#### Prerequisites
+- Node v10.15 +
+- NPM
+
+#### Install
+```
+npm i
+```
+
+#### Run
+```
+npm start
+```
---
+++ chatbot-ui/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>M&S Bank</h1>
+      </header>
+      <main>
+        <p>Lorem ipsum</p>
+      </main>
+    </div>
+  );
+}
+
+export default App;
