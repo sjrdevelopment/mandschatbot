37a762c437d6cd398d7382b85871aed7f8ead9c4
sjrdevelopment <stuj.robinson@gmail.com>
2019-10-28 22:36:03 +0000

    Chat message logic in UI

--- chatbot-ui/src/App.js
+++ chatbot-ui/src/App.js
@@ -1,27 +1,79 @@
-import React from 'react';
+import React, { useState, useReducer } from 'react';
 import './App.css';
 
 function App() {
+
+  const initMessages = initialMessages => {
+    return initialMessages;
+  }
+
+  const reducer = (state, action) => {
+    switch (action.type) {
+      case 'userMsg':
+        return [
+          ...state,
+          {
+            author: 'user',
+            content: action.payload
+          }
+        ]
+        break
+      case 'sysMsg':
+        return [
+          ...state,
+          {
+            author: 'system',
+            content: action.payload
+          }
+        ]
+        break
+      default:
+        throw new Error();
+    }
+  }
+
+  const initialMessages = [{author: 'system', content: 'test message 2 from system'}, {author: 'user', content: 'test message 2 from user'}]
+  const [state, dispatch] = useReducer(reducer, initialMessages, initMessages);
+  const [currentMessage, setCurrentMessage] = useState('')
+
+  const showMessage = message => (
+    <div className={`chatbox chatbox-${message.author}`}>
+      <p>{message.content}</p>
+    </div>
+  )
+  
+  const respondFromSystem = () => {
+    setTimeout(() => {
+     dispatch({type: 'sysMsg', payload: 'test response from system'})
+    }, 1000)
+  }
+
+  const writeUserMessage = async (currentMessage) => {
+    await dispatch({type: 'userMsg', payload: currentMessage})
+    respondFromSystem()
+  }
+
+  const handleSubmit = (event, currentMessage) => {
+    event.preventDefault();
+
+    writeUserMessage(currentMessage)
+    setCurrentMessage('')
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>M&S Bank</h1>
       </header>
       <main>
-        <p>Lorem ipsum</p>
         <section class="chat-area">
-          <div className="chatbox chatbox-ms">
-            <p>Hello world</p>
-          </div>
-          <div className="chatbox chatbox-user">
-            <p>Hello world user</p>
-          </div>
+          {state.map(message => showMessage(message))}
         </section>
 
         <div className="message-author">
-          <form name="">
+          <form name="" onSubmit={(event) => { handleSubmit(event, currentMessage) }} >
             <label for="message-input">Enter your message</label>
-            <input id="message-input" type="text" />
+            <input id="message-input" type="text" value={currentMessage} onChange={(event) => {setCurrentMessage(event.target.value)}} />
           </form>
         </div>
       </main>
